66f383e3ea00eb0d1a4a771545720efba6d1b14a
maiktreya <garciaduchm@gmail.com>
2025-12-23 12:08:08 +0100

    implemented

--- src/IPC_alquiler.R
+++ src/IPC_alquiler.R
@@ -4,11 +4,10 @@
 suppressPackageStartupMessages({
     library(data.table)
     library(survey)
-    library(magrittr)
 })
 
 # Waves included
-period <- c(19:29)
+period <- c(19:24)
 
 # Accumulators
 obj <- c()
@@ -16,21 +15,20 @@ summary_table <- data.table()
 
 for (year in period) {
     # Load per<U+2011>year mean<U+2011>imputed microdata
-    file_path <- sprintf("out/%.csv", year)
+    file_path <- sprintf("out/%d.gz", year)
     ecv <- fread(file_path)
 
     # Ensure types
     ecv[, weights := as.numeric(DB090)]
-    ecv[, renta_alquiler := as.numeric(p2_31)]
-    ecv[, reg_tenencia := factor(p2_19)]
+    ecv[, renta_alquiler := as.numeric(HY030N)]
+    ecv[, reg_tenencia := factor(HH021)] # valor 3 para alquiler pm
 
-    dt <- svydesign(ids = ~1, data = ecv, weights = ecv$DB090)
+    dt <- svydesign(ids = ~1, data = ecv, weights = ecv$weights)
 
     # Compute quantiles
     quant <- svymean(~renta_alquiler, subset(dt, reg_tenencia == "1"), na.rm = TRUE)
     obj <- c(obj, quant[[1]])
 }
 
-names(obj) <- period
-
-rental_grp <- diff(log(obj))
+results <- data.table(period = c(2019:2024), rental_p = obj, rental_grp = c(NA, diff(log(obj))))
+print(results)
