89e1737cfef0c185993635bc215a2c6e435aad1f
maiktreya <garciaduchm@gmail.com>
2025-12-23 12:32:02 +0100

    last

--- src/IPC_alquiler.R
+++ src/IPC_alquiler.R
@@ -1,5 +1,4 @@
-# Compute, for each survey wave, the share of total population and the share of
-# total net wealth held by selected quantiles groups
+# Compute, for each survey wave, the mean household payment on main residence for renteds
 
 suppressPackageStartupMessages({
     library(data.table)
@@ -11,24 +10,26 @@ period <- c(19:24)
 
 # Accumulators
 obj <- c()
-summary_table <- data.table()
 
+# loop over waves
 for (year in period) {
     # Load per<U+2011>year mean<U+2011>imputed microdata
     file_path <- sprintf("out/%d.gz", year)
     ecv <- fread(file_path)
 
-    # Ensure types
+    # Ensure types & names
     ecv[, weights := as.numeric(DB090)]
-    ecv[, renta_alquiler := as.numeric(HY030N)]
-    ecv[, reg_tenencia := factor(HH021)] # valor 3 para alquiler pm
+    ecv[, renta_alquiler := as.numeric(HH060)]
+    ecv[, reg_tenencia := factor(HH021)] # valor 3 para alquiler pm 
 
+    # Create survey design
     dt <- svydesign(ids = ~1, data = ecv, weights = ecv$weights)
 
     # Compute quantiles
-    quant <- svymean(~renta_alquiler, subset(dt, reg_tenencia == "1"), na.rm = TRUE)
+    quant <- svymean(~renta_alquiler, subset(dt, reg_tenencia == "3"), na.rm = TRUE)
     obj <- c(obj, quant[[1]])
 }
 
+# Print results
 results <- data.table(period = c(2019:2024), rental_p = obj, rental_grp = c(NA, diff(log(obj))))
 print(results)
